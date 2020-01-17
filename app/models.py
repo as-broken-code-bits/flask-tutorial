3356c48beca5113fe170e740af1af47fe0b632e8
arsummers <aliyasummers1@gmail.com>
2020-01-17 15:40:36 -0800

    following functions in model

--- app/models.py
+++ app/models.py
@@ -25,6 +25,18 @@ class User(UserMixin, db.Model):
         secondaryjoin=(followers.c.followed_id == id),
         backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.append(user)
+
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
