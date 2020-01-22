b09bd1be029feea77e7185cd571fedcc4fd49872
arsummers <aliyasummers1@gmail.com>
2020-01-21 16:48:38 -0800

    only handle real posts

--- app/routes.py
+++ app/routes.py
@@ -17,16 +17,8 @@ def index():
         db.session.commit()
         flash('Your post is now live')
         return redirect(url_for('index'))
-    posts = [
-        {
-            'author' : {'username': 'John'},
-            'body' : 'John says things'
-        },
-        {
-            'author' : {'username' : 'Susan'},
-            'body' : 'Susan says things'
-        }
-    ]
+    posts = current_user.followed_posts().all()
+
     return render_template("index.html", title='Home Page', form=form, posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
